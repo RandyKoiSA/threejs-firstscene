771770cf0108bc0980058d5b3e5371aaf5f9c2ab
Randy Le <97randy.le@gmail.com>
2023-07-08 06:41:11 -0700

    test

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/browse/three@0.77.0/';
+import * as THREE from 'https://unpkg.com/browse/three@0.77.0/three.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
